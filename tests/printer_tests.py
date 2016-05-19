c0c587dced5ffba3d8582f4fc36ea19506cfae92
Eric Appelt <eric.appelt@gmail.com>
2016-05-18 20:56:14 -0500

    Add test for unchurch_list
    Add a test for unchurch_list using an independent
    single pair Church list implementation.

--- tests/printer_tests.py
+++ tests/printer_tests.py
@@ -7,16 +7,63 @@ differing Church list implementations
 """
 import unittest
 import unittest.mock
+from collections import namedtuple
+
 from fizzbuzz.printer import (
     unchurch,
     unchurch_list
 )
 
+ListImpl = namedtuple('ListImpl', ['cons', 'head', 'tail', 'nil', 'isnil'])
+
 
 class TestPrinter(unittest.TestCase):
 
     def test_unchurch(self):
+        """
+        Basic tests for the unchurch method
+        """
         zero = lambda f: lambda x: x
         three = lambda f: lambda x: f(f(f(x)))
         self.assertEqual(unchurch(zero), 0)
         self.assertEqual(unchurch(three), 3)
+
+    def test_unchurch_list_implementation_a(self):
+        """
+        Test the unchurch list function using list implementation a
+        defined in this module.
+        """
+        impl = self._list_implementation_a()
+        zero = lambda f: lambda x: x
+        one = lambda f: lambda x: f(x)
+        two = lambda f: lambda x: f(f(x))
+        three = lambda f: lambda x: f(f(f(x)))
+        a0 = impl.cons(zero)(impl.nil)
+        a1 = impl.cons(one)(a0)
+        a2 = impl.cons(two)(a1)
+        a3 = impl.cons(three)(a2)
+
+        ul = unchurch_list(a3, head=impl.head, tail=impl.tail, isnil=impl.isnil)
+        
+        self.assertEqual([3, 2, 1, 0], [unchurch(elem) for elem in ul])
+
+    def _list_implementation_a(self):
+        """
+        Return a Church list implementation for using in tests
+
+        This implementation uses a single pair. This implementation
+        was written without copy/paste from any solution code.
+        """
+        true = lambda a: lambda b: a
+        false = lambda a: lambda b: b
+        pair = lambda x: lambda y: lambda z: (z)(x)(y)
+        first = lambda p: p(true)
+        second = lambda p: p(false)
+        isnil = lambda l: l(lambda h: lambda t: lambda d: false)(true)
+        return ListImpl(
+            cons=pair,
+            head=first,
+            tail=second,
+            nil=false,
+            isnil=isnil
+        )
